0b5b11921337ac665edbbae07a5afe31615d3a35
StreamPay <contact@streamprotocol.org>
2023-02-27 10:50:50 +0200

    commit

---
+++ src/components/templates/login-layout.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+const LoginLayout = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="grid grid-cols-1 grid-rows-1 min-h-screen">
+        <div
+          className="flex flex-col items-center"
+          style={{
+            background: "linear-gradient(90deg, #0968E5 0%, #091970 100%)",
+          }}
+        >
+          {children}
+          <div className="text-grey-0 inter-base-regular pb-12">
+            © 2023 Stream Protocol / StreamPay™ <span>&#183;</span>{" "}
+            <a
+              style={{ color: "inherit", textDecoration: "none" }}
+              href="mailto:contact@streamprotocol.org"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginLayout
